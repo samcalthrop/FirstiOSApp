81970b288195ba8f7e9e67710ed895dc5f69216a
Sam Calthrop <sam@calthrop.com>
2024-04-19 10:03:58 +0100

    Initial Commit

---
+++ FirstiOSApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FirstiOSApp
+//
+//  Created by Sam Calthrop on 19/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstiOSApp/FirstiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstiOSAppApp.swift
+//  FirstiOSApp
+//
+//  Created by Sam Calthrop on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FirstiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
